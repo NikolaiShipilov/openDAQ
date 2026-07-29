dce7db727bfe6ae37203b25b3cfc8b9ca71caa62
Nikolai Shipilov <nikolai.shipilov@opendaq.com>
2026-07-29 14:34:50 +0200

    Add missing include

--- core/opendaq/opendaq/include/opendaq/opendaq.h
+++ core/opendaq/opendaq/include/opendaq/opendaq.h
@@ -103,6 +103,7 @@
 #include <opendaq/device_update_options_factory.h>
 
 #include <opendaq/credential_provider_factory.h>
+#include <opendaq/credential_request_factory.h>
 
 #include <opendaq/exceptions.h>
 
